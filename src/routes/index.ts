5eef73237bebebaf6f72e9c2fee7e771c2f10dc8
jasmin030303 <jasmintagaeva03@gmail.com>
2025-10-30 14:50:23 +0600

    update backend feature

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ const globalRouter = Router();
 const corsConfig = {
   origin: ["http://localhost:3000"],
 };
+
 globalRouter.use("/todo", cors(corsConfig), todoRoutes);
 
 export default globalRouter;
